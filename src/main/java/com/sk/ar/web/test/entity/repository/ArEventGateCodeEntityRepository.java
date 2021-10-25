fef86d7cbbf4abee380c6e393c4a907d0fec2ffa
anjiho <anjo0080@gmail.com>
2021-10-25 19:23:00 +0900

    fixed untracked files

--- src/main/java/com/sk/ar/web/test/entity/repository/ArEventGateCodeEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sk.ar.web.test.entity.repository;
-
-import com.sk.ar.web.test.entity.ArEventGateCodeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArEventGateCodeEntityRepository extends JpaRepository<ArEventGateCodeEntity, Long> {
-    void deleteByEventId(String eventId);
-}
--- src/main/java/com/sk/ar/web/test/entity/repository/ArEventLogicalEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sk.ar.web.test.entity.repository;
-
-import com.sk.ar.web.test.entity.ArEventLogicalEntity;
-import com.sk.ar.web.test.entity.ArEventObjectEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArEventLogicalEntityRepository extends JpaRepository<ArEventLogicalEntity, Integer> {
-    ArEventLogicalEntity findByArEventId(int eventId);
-}
